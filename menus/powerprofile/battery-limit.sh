29e6a507ff45388a230150bb26749e4cecc4d73c
MrachkovskyiOleksandr <oleksandrmrackovskyj@gmail.com>
2026-02-22 00:30:43 +0100

    added info about current power profile and battery charge limit

--- menus/powerprofile/battery-limit.sh
+++ menus/powerprofile/battery-limit.sh
@@ -5,11 +5,19 @@ style="
   element-icon { enabled: false;} 
 "
 
-options=$(printf "󰜃 | One-shot\n󰁹 | 100%%\n󰂂 | 90%%\n󰂁 | 80%%" | rofi -dmenu -p "Battery limit" -theme-str "$style")
+# Battery limit info
+bat_limit=$(asusctl battery info)
+
+# Rofi dmenu assembly
+options=$(printf "󰜃 | One-shot\n󰁹 | 100%%\n󰂂 | 90%%\n󰂁 | 80%%" | rofi -dmenu -p "Battery limit" -mesg "$bat_limit" -theme-str "$style")
+
+notification() {
+  notify-send "Battery limit" "Battery limit is set to: $1"
+}
 
 case "$options" in
-  *"One"*) asusctl battery oneshot ;;
-  *"100"*) asusctl battery limit 100 ;;
-  *"90"*) asusctl battery limit 90 ;;
-  *"80"*) asusctl battery limit 80 ;;
+  *"One"*) asusctl battery oneshot | notification "One-shot" ;;
+  *"100"*) asusctl battery limit 100 | notification "100%" ;;
+  *"90"*) asusctl battery limit 90 | notification "90%" ;;
+  *"80"*) asusctl battery limit 80 | notification "80%" ;;
 esac
--- menus/powerprofile/powerprofiles.sh
+++ menus/powerprofile/powerprofiles.sh
@@ -5,11 +5,30 @@ style="
   element-icon { enabled: false;} 
 "
 
-options=$(printf "󰌪 | Power saver\n󰾅 | Balanced\n | Performance\n󰂄 | Battery limit" | rofi -dmenu -p "Power profile" -theme-str "$style")
+# Power profile info
+pow_prof=$(powerprofilesctl get)
+case "$pow_prof" in 
+  "power-saver") value="Power saver" ;;
+  "balanced") value="Balanced" ;;
+  "performance") value="Performance" ;;
+esac
+
+# Usefull info about battery and power profile
+message=$(printf "Current power profile: $value")
+
+
+# Rofi dmenu assembly
+list="󰌪 | Power saver\n󰾅 | Balanced\n | Performance\n󰂄 | Battery limit"
+
+options=$(printf "$list" | rofi -dmenu -p "Power profile" -mesg "$message" -theme-str "$style")
+
+notification() {
+  notify-send "Power profile" "Power profile is set to: $1"
+}
 
 case "$options" in
-  *"saver") powerprofilesctl set power-saver ;;
-  *"Balanced") powerprofilesctl set balanced ;;
-  *"Performance") powerprofilesctl set performance ;;
+  *"saver") powerprofilesctl set power-saver | notification "Power saver" ;;
+  *"Balanced") powerprofilesctl set balanced | notification "Balanced" ;;
+  *"Performance") powerprofilesctl set performance | notification "Performance" ;;
   *"limit") "$HOME/.config/rofi/menus/powerprofile/battery-limit.sh" ;;
 esac
